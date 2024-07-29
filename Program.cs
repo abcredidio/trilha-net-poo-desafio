cc55d8a2aed72dfa43b00abbc3dfba51aac52967
André Credidio <andrebcredidio@gmail.com>
2024-07-29 14:54:36 -0300

    Finalizando Projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("8199999-7979", "iphone15", "897979456464646", 256);
+Nokia nokia = new Nokia("8199999-7978", "Nokia 1300", "1231564897434", 16);
+
+Console.WriteLine("Testando Nokia:");
+nokia.InfosAparelho();
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+Console.WriteLine("----------------------------");
+Console.WriteLine("Testando Iphone");
+iphone.InfosAparelho();
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
